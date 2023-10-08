c2aca524654d1495041defef8b78f79fa4e65986
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2023-10-08 18:17:22 -0500

    feat: add credentials and validation email

--- index.js
+++ index.js
@@ -8,29 +8,42 @@ app.use(bodyParser.json());
 
 app.post("/send-email", function (req, res) {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.ethereal.email",
     port: 587,
     auth: {
-      user: "simongonzalezechavarria@gmail.com",
-      pass: "$imon0610",
+      user: "yasmeen26@ethereal.email",
+      pass: "XPHK7Y27b4GHHcDqS5",
     },
   });
 
   const mailOptions = {
     form: "Remitente",
     to: req.body.email,
-    subject: "Solicitud de contacto.",
+    subject: "Solicitud de contacto",
     text: `Hola Simon, soy ${req.body.name}, mi numero es ${req.body.phone} y me gustaría contactarme contigo. Mi mensaje es el siguiente: ${req.body.message}`,
   };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      res.send(500, err.message);
+  transporter.sendMail(mailOptions, (error) => {
+    if (req.body.email === "" || req.body.email === undefined || req.body.email === null) {
+      res.status(400).jsonp({
+        status: "error",
+        message: "El nombre es obligatorio." 
+      });
     } else {
-      console.log("Email sent");
-      res.status(200).jsonp(req.body);
+      if (error) {
+      console.log(error);
+      res.status(500).jsonp({
+        status: "error",
+        message: "Error al enviar el correo, intentalo de nuevo." 
+      });
+    } else {
+      res.status(200).jsonp({
+        status: "success",
+        message: "Solicitud Enviada correctamente, pronto me pondré en contacto contigo." 
+      });
     }
+    }
+    
   });
 });
 
