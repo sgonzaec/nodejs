83ace62a7c4d31c41ed3a42d6e72ff6783565a8e
Simon Gonzalez <30642867+sgonzaec@users.noreply.github.com>
2023-10-08 17:59:39 -0500

    feat: custom endpoints

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-const http = require('http');
-const PORT = 3000;
+const express = require("express");
+const nodemailer = require("nodemailer");
+const bodyParser = require("body-parser");
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post("/send-email", function (req, res) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    auth: {
+      user: "simongonzalezechavarria@gmail.com",
+      pass: "$imon0610",
+    },
+  });
+
+  const mailOptions = {
+    form: "Remitente",
+    to: req.body.email,
+    subject: "Solicitud de contacto.",
+    text: `Hola Simon, soy ${req.body.name}, mi numero es ${req.body.phone} y me gustaría contactarme contigo. Mi mensaje es el siguiente: ${req.body.message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send(500, err.message);
+    } else {
+      console.log("Email sent");
+      res.status(200).jsonp(req.body);
+    }
+  });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(3000, function () {
+  console.log("server running on port 3000");
 });
